d610aaa3ef682efcd0aa2e745398f6960a0d2876
parkermuir <parker.muir@gmail.com>
2018-07-13 14:59:09 -0400

    Search input form is working
    -onSubmit capturing event, updating state and ready to call to server

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,38 @@ import React from 'react';
 class Search extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      team: ''
+    };
+    this.search = this.search.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  search(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    this.props.onSearch(this.state.team);
+  }
+
+  onChange(e) {
+    this.setState({
+      team: e.target.value
+    });
   }
 
   render() {
     return (
-      <input className="input" type="text" placeholder="Enter NBA Team"/>
+      <div>
+        <form onSubmit={this.search}>
+          <input className="input" type="text" placeholder="Enter NBA Team Abbreviation ex: BOS"
+            value={this.state.team.slice(0, 3).toUpperCase()} onChange={this.onChange}>
+          </input>
+        </form>
+
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './components/Search.jsx';
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     //state here
+
+    this.onSearch = this.onSearch.bind(this);
+  }
+
+  onSearch(team) {
+    //hit up express for that team
+    console.log(team, ' was searched');
+
   }
 
   render() {
@@ -13,7 +22,7 @@ class App extends React.Component {
       <section className="section">
         <div className="container">
           <h1 className="title">Query The NBA</h1>
-          <Search />
+          <Search onSearch={this.onSearch} />
         </div>
       </section>
     );
